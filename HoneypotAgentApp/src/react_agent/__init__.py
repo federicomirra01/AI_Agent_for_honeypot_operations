75ee2b637d358b90f888d6c47295d73c315e1394
federicomirra01 <federicomirra01@gmail.com>
2025-05-12 12:38:54 +0200

    fix

--- HoneypotAgentApp/src/react_agent/__init__.py
+++
@@ -1,9 +0,0 @@
-"""React Agent.
-
-This module defines a custom reasoning and action agent graph.
-It invokes tools in a simple loop.
-"""
-
-from HoneypotAgentApp.src.react_agent.graph_react import graph
-
-__all__ = ["graph"]
