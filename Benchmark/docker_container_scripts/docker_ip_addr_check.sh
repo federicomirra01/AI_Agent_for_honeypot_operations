2cd487b2a75b2d3d62d7383b29c351ffe37aca4b
federicomirra01 <federicomirra01@gmail.com>
2025-07-29 18:21:19 +0200

    fix

--- Benchmark/docker_container_scripts/docker_ip_addr_check.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-docker ps -q | xargs -n1 docker inspect --format='{{.Name}} - IP: {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}} - Ports: {{range $p, $conf := .NetworkSettings.Ports}}{{$p}}{{if $conf}} => {{range $conf}}{{.HostIp}}:{{.HostPort}} {{end}}{{end}}{{end}}'
-
--- Benchmark/docker_container_scripts/remove_all_containers.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-cd ~/Desktop/Thesis/AI_Agent_for_honeypot_operations/Benchmark/vulnerableContainers/cves/cve-2018-12613 && docker compose down -v
-cd ~/Desktop/Thesis/AI_Agent_for_honeypot_operations/Benchmark/vulnerableContainers/multi_step_machine/docker/unauthorized-rce && docker compose down -v
-cd ~/Desktop/Thesis/AI_Agent_for_honeypot_operations/Benchmark/vulnerableContainers/multi_step_machine/struts2/s2-057 && docker compose down -v
-cd ~/Desktop/Thesis/AI_Agent_for_honeypot_operations/Benchmark/vulnerableContainers/single_step_machine/gitlab/CVE-2021-22205 && docker compose down -v
-#cd ~/Desktop/Thesis/AI_Agent_for_honeypot_operations/Benchmark/firewallContainer && docker compose down -v
-#cd ~/Desktop/Thesis/AI_Agent_for_honeypot_operations/Benchmark/attackerContainer && docker compose down -v
