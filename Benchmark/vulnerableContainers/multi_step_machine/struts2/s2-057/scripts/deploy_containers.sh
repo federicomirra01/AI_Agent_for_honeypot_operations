fac337a5a566384ba08c8fd831b8240cf5d6d6fc
federicomirra01 <federicomirra01@gmail.com>
2025-08-06 18:15:34 +0200

    code refactoring

--- Benchmark/vulnerableContainers/multi_step_machine/struts2/s2-057/scripts/deploy_containers.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-docker compose up -d
-# Insert second stage flag into container
-docker exec -u 0 s2-057-struts2-1 sh -c 'echo "flag{second_stage_completed}" > /root/root.txt'
--- Benchmark/vulnerableContainers/multi_step_machine/struts2/s2-057/scripts/remove_containers.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-docker stop s2-057-struts2-1
-docker rm s2-057-struts2-1
