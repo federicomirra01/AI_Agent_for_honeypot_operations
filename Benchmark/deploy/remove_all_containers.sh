54580e4296e4a56e077e732b7e498b82509c6c2d
federicomirra01 <federicomirra01@gmail.com>
2025-08-07 10:27:05 +0200

    Deploy script

---
+++ Benchmark/deploy/remove_all_containers.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+cd ~/Desktop/Thesis/AI_Agent_for_honeypot_operations/Benchmark/vulnerableContainers/exploitable/xdebug-rce && docker compose down -v
+cd ~/Desktop/Thesis/AI_Agent_for_honeypot_operations/Benchmark/vulnerableContainers/exploitable/docker/unauthorized-rce && docker compose down -v
+cd ~/Desktop/Thesis/AI_Agent_for_honeypot_operations/Benchmark/vulnerableContainers/exploitable/struts2/s2-057 && docker compose down -v
+cd ~/Desktop/Thesis/AI_Agent_for_honeypot_operations/Benchmark/vulnerableContainers/exploitable/gitlab/CVE-2021-22205 && docker compose down -v
+cd ~/Desktop/Thesis/AI_Agent_for_honeypot_operations/Benchmark/vulnerableContainers/decoys/activemq/CVE-2015-5254 && docker compose down -v
+cd ~/Desktop/Thesis/AI_Agent_for_honeypot_operations/Benchmark/vulnerablContainers/decoys/bash/CVE-2014-6271 && docker compose down -v
+cd ~/Desktop/Thesis/AI_Agent_for_honeypot_operations/Benchmark/firewallContainer && docker compose down -v
+cd ~/Desktop/Thesis/AI_Agent_for_honeypot_operations/Benchmark/attackerContainer && docker compose down -v
