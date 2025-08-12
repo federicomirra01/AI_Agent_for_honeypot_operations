ba68e2056dc3d924deb494beba803552c8d86646
federicomirra01 <federicomirra01@gmail.com>
2025-08-12 10:56:12 +0200

    Attack Phase staging

---
+++ Benchmark/attackerContainer/cleanup_logs.sh
@@ -0,0 +1,1 @@
+sudo rm -r /home/c0ff3k1ll3r/Desktop/Thesis/AI_Agent_for_honeypot_operations/Benchmark/attackerContainer/state/*
--- Benchmark/deploy/remove_vulnerable_containers.sh
+++ Benchmark/deploy/remove_vulnerable_containers.sh
@@ -4,4 +4,5 @@ cd ~/Desktop/Thesis/AI_Agent_for_honeypot_operations/Benchmark/vulnerableContain
 cd ~/Desktop/Thesis/AI_Agent_for_honeypot_operations/Benchmark/vulnerableContainers/exploitable/struts2/s2-057 && docker compose down -v
 cd ~/Desktop/Thesis/AI_Agent_for_honeypot_operations/Benchmark/vulnerableContainers/exploitable/gitlab/CVE-2021-22205 && docker compose down -v
 cd ~/Desktop/Thesis/AI_Agent_for_honeypot_operations/Benchmark/vulnerableContainers/decoys/activemq/CVE-2015-5254 && docker compose down -v
-cd ~/Desktop/Thesis/AI_Agent_for_honeypot_operations/Benchmark/vulnerablContainers/decoys/bash/CVE-2014-6271 && docker compose down -v
+cd ~/Desktop/Thesis/AI_Agent_for_honeypot_operations/Benchmark/vulnerableContainers/decoys/bash/CVE-2014-6271 && docker compose down -v
+
